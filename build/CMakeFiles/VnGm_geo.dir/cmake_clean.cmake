e201df688791553468c3d6bc02a95a3874d469f4
ptanhdnn <79252891+ptanhdnn@users.noreply.github.com>
2023-11-17 19:06:54 +0700

    Delete build directory

--- build/CMakeFiles/VnGm_geo.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/VnGm_geo"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/VnGm_geo.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
